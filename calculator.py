e63bdc4ba3cc1f6ba016bc7e6f61c000b8fb745b
Jagan Rajagopal <jaganrajagopal@outlook.com>
2023-01-31 11:25:30 +0530

    python project for jenkins

---
+++ calculator.py
@@ -0,0 +1,22 @@
+"""
+Calculator library containing basic math operations.
+"""
+import pytest
+import sys
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+if __name__ == '__main__':
+    rc = 1
+    try:
+        print("addition method")
+        print(add(2,3))
+        rc = 0
+    except Exception as e:
+        print('Error: %s' % e, file=sys.stderr)
+    sys.exit(rc)
+
+
